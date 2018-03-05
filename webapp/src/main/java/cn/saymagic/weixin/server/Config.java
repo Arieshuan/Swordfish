7e63501190f1e77e4a84cc0ea7e06eb803a0a34f
Aries <huang_zhou88@foxmail.com>
2018-03-05 11:18:26 +0800

    Update Config.java

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -6,6 +6,6 @@ public class Config {
 	public static final String TOKEN = "";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "6c07a8b88f784d24bbba038149add892"; 
 	
 }
